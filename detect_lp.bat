2b2cd6d259253703134281468b148c1280ebbe27
Sungmin Chun <schun92@gmail.com>
2022-11-29 23:30:11 +0900

    Add detect_lp.bat and data/LPR.yaml

---
+++ detect_lp.bat
@@ -0,0 +1,2 @@
+#python detect.py --weights j:\WORK\yolov5\runs\train\exp\weights\best.pt --save-crop --max-det 1 --classes 1 --conf-thres=0.7 --img 320 --source 16422962795750963.jpg
+python detect.py --weights d:\WORK\LPR\yolov5\runs\train\EXP_320\weights\best.pt --data=data\LPR.yaml --save-crop --max-det 1 --classes 1 --conf-thres=0.5 --img 320 --source e:\LPR_Images\images
